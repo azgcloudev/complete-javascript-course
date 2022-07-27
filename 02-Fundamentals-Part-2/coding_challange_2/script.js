3045bfc092bab409f96b14ba335529dc03f1a0ca
Aldair Zamora <aldair.zamora@outlook.com>
2022-07-27 00:17:02 -0600

    study arrays and objects

---
+++ 02-Fundamentals-Part-2/coding_challange_2/script.js
@@ -0,0 +1,40 @@
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+*/
+
+const calcTip = (billValue) => {
+    if (billValue >= 50 && billValue <= 300) {
+        // 15% tip
+        return (billValue * 0.15) + billValue;
+    } else {
+        // 20% tip
+        return (billValue * 0.20) + billValue;
+    }
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+let bilssTotal = bills.reduce((previous, current) => {
+    previous += current;
+    return previous;
+});
+
+let tipsTotal = tips.reduce((previous, current) => {
+    previous += current;
+    return previous;
+});
+
+
+let total = bilssTotal + tipsTotal;
+console.log(tips)
+console.log('total is', total);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -97,3 +97,80 @@ console.log(friends[friends.length - 1]);	// print last array element
 friends[2] = 'Jay';
 console.log(friends[2]);
 
+
+
+
+// >>>>>>>>>>> ARRAYS methods <<<<<<<<<<<<
+const friends2 = ['Michael', 'Steven', 'Peter'];
+
+// arr.push()	--> add elements to the end of the array
+// push return the length of the array
+let newArrLen = friends2.push('Jay');
+console.log('push() return: ', newArrLen);
+
+// add elements to the beginning of the array shift()
+friends2.unshift('John');
+console.log('friends2', friends2);
+
+// remove the last element from the array pop()
+// returns the element deleted
+console.log(friends2.pop());
+
+//remove the first element of an array shift()
+friends2.shift();
+console.log(friends2);
+
+// of an array element
+console.log(friends2.indexOf('Steven'));
+console.log(friends2.indexOf('Aldair')); // if the search cannot find the value will return -1
+
+// if the value is or not in the array includes()
+console.log(friends2.includes('Peter'));
+
+
+
+
+
+
+// >>>>>>>>>> Objects <<<<<<<<<<
+const aldair = {
+	firstName: 'Aldair',
+	lastName: 'Zamora',
+	age: 2022 - 1995,
+	job: 'Support engineer',
+	friends: ['Diego', 'Connie', 'Pancho']
+}
+console.log('\n*********************************');
+console.log(aldair)
+console.log(aldair.firstName); // dot notation have to use the property name
+console.log(aldair['lastName']); // bracket uses statements
+
+let interestedIn = prompt("Type one option only between: firstName, lastName, age, job or friends");
+
+if (aldair[interestedIn]) {
+	console.log(aldair[interestedIn]);
+} else {
+	interestedIn = prompt('Please enter the correct value. Type one option only between: firstName, lastName, age, job or friends');
+}
+
+
+// you can get undefined if try to access a key from an object that does not exists
+
+//add a new key pair value
+aldair.location = 'Costa Rica';
+aldair['github'] = 'azgcloudev';
+
+// ---- challange ----
+/*
+"Aldair has 3 friends, and his best friend is called Diego"
+
+Try to console the phrase but use values from an object
+*/
+
+console.log(`${aldair.firstName} has ${aldair.friends.length} friends, and his best friend is called ${aldair.friends[0]}.`);
+
+
+
+
+
+// >>>>>>>>>> OJECTS METHODS <<<<<<<<<<
