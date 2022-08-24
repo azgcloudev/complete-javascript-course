135f394b7d5bf04a185a2c7644255d129fbc3fb3
Aldair Zamora <aldair.zamora@outlook.com>
2022-08-23 22:54:13 -0600

    start to study DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 21;
+
+document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+    let message = document.querySelector('.message');
+	let guess = Number(document.querySelector('.guess').value);
+
+	if (!guess) {
+		message.textContent = 'No Number added';
+	}
+});
