0c8d6a07ec4602ad4b0cda108f4b3e790986dc16
Aldair Zamora <aldair.zamora@outlook.com>
2022-10-09 22:11:59 -0700

    complete game guess number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,62 @@
 'use strict';
 
+// generate secret number from 1 to 20
+const secretNumber = Math.floor(Math.random() * 20 + 1);
 
+// total score
+let score = 20;
+let highscore = 0;
+
+// what to do when the check button is clicked
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
-    console.log(guess);
 
+    // when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number!';
+        // when player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '✅ Correct number!'
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
+        // when guess is to high
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high!'
+        score--;
+        if (score > 0) {
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = "😔 You lost the game"
+        }
+        // when guess is to low
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low!'
+        score--;
+        if (score > 0) {
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = "😔 You lost the game!"
+        }
     }
-})
+});
+
+
+// reset the game with the 'again' button
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'Start guessing...';
+});
