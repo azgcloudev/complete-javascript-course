10f1cc82d8c351c16f746147b57a78f0c98c4d7f
Aldair Zamora <aldair.zamora@outlook.com>
2023-03-28 18:33:47 -0600

    reviewing block and function variables scopes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const print = string => { console.log(string)}
+
+function calcAge(birthDay) {
+    const age = 2023 - birthDay;
+    
+    function printAge() {
+        console.log(`${firstName} is ${age}, born in ${birthDay}`);
+    }
+
+    printAge();
+
+    return age;
+}
+
+const firstName = "Jonas";
+calcAge(1991);
